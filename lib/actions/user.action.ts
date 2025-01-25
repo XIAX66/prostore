6ae51a32a1eefbfcc78480fcd5814b96f7f8c557
夏宇晗 <xiayh66@gmail.com>
2025-01-25 15:49:25 +0800

    2024-1-25(2)

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
+import { formatError } from "../utils";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -62,11 +63,10 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
 
     return { success: true, message: "注册成功" };
   } catch (error) {
-    console.error(error);
     if (isRedirectError(error)) {
       throw error;
     }
 
-    return { success: false, message: "注册失败" };
+    return { success: false, message: formatError(error) };
   }
 }
